8bc11377d04a58e16a4d03d045cf417679f77c67
ryan@bariserver.com <ryan@bariserver.com@aeaa4eaa-a835-11dd-a887-7ffe1a420f8d>
2008-11-02 00:29:35 +0000

    Initial endorsements app

---
+++ trunk/envirovote/endorsements/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from races.models import Candidate
+
+# Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(blank=True,null=True)
+    #state = models.CharField(max_length=2)
+    #diff between fringe groups vs mainstream groups?
+    
+    def __unicode__(self):
+        return self.name
+        
+class Endorsement(models.Model):
+    organization = models.ForeignKey(Organization)
+    candidate = models.ForeignKey(Candidate)
+    url = models.URLField(blank=True,null=True)
+    # positive vs negitive endorsements? dirty dozen?
+    
+    def __unicode__(self):
+        return self.organization.name+" endorses "+self.candidate.name
+    
