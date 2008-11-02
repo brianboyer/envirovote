4a8057ea5f34945201b82345e595dce123315d9c
benderbending <benderbending@aeaa4eaa-a835-11dd-a887-7ffe1a420f8d>
2008-11-02 00:25:53 +0000

    added vote info to key races and candidates

--- envirovote/races/models.py
+++ envirovote/races/models.py
@@ -16,23 +16,21 @@ class Race(models.Model):
     headline = models.CharField(max_length=200, blank=True)
     deck = models.CharField(max_length=200, blank=True)
     body = models.TextField(blank=True)
-    #results - array?, no, vote count on candidate
-    #winner - no, on candidate
-    #percent reported
-    #get greenness from candidate
+    percent_vote_reported = models.IntegerField(blank=True, null=True)
+    vote_last_updated = models.DateTimeField(blank=True, null=True)
+    winner = models.ForeignKey("Candidate", blank=True, null=True, related_name="won")
+    projected = models.BooleanField()
 
     def __unicode__(self):
         return "%s %s" % (self.state, self.race_type)
-    
+
 class Candidate(models.Model):
     name = models.CharField(max_length=200)
     photo = models.URLField(blank=True)
     race = models.ForeignKey(Race)
     is_key = models.BooleanField()
-    incumbent = models.BooleanField()
     last_elected = models.IntegerField(blank=True, null=True)
-    #votes = models.IntegerField(blank=True, null=True)
-    #is_winner = models.BooleanField
+    votes = models.IntegerField(blank=True, null=True)
     
     #links = models.TextField(blank=True) ??
     #political courage test results
