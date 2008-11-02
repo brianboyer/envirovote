c2d11ab69961381b0eee1a64d02ecbf6e38c664e
benderbending <benderbending@aeaa4eaa-a835-11dd-a887-7ffe1a420f8d>
2008-11-02 03:34:46 +0000

    first rev of endorse-o-meter

--- trunk/envirovote/races/views.py
+++ trunk/envirovote/races/views.py
@@ -5,4 +5,15 @@ from endorsements.models import *
 def index(request):
     key = Race.objects.filter(is_key=True)
     incoming = Race.objects.filter(winner__isnull=False).order_by('-tally_updated')[:10]
-    return render_to_response('index.html', {'key_races': key, 'incoming_races': incoming,})
+    total_races = 0
+    decided_races = 0
+    green_races = 0
+    for race in Race.objects.all():
+        total_races += 1
+        if race.winner:
+            decided_races += 1
+            if race.winner == race.greenest:
+                green_races += 1
+    percent_green = 100 * float(green_races) / float(decided_races)
+    remaining_races = total_races - decided_races
+    return render_to_response('index.html', {'key_races': key, 'incoming_races': incoming, 'decided_races': decided_races, 'green_races': green_races, 'percent_green': percent_green, 'remaining_races': remaining_races,})
