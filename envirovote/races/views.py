21561c07e9c766804964e38884f9b2e1e403404c
benderbending <benderbending@aeaa4eaa-a835-11dd-a887-7ffe1a420f8d>
2008-11-02 05:17:42 +0000

    refactored how we calculate meter info a bit

--- envirovote/races/views.py
+++ envirovote/races/views.py
@@ -6,39 +6,34 @@ from endorsements.models import *
 def index(request):
     key = Race.objects.filter(is_key=True)
     incoming = Race.objects.filter(winner__isnull=False).order_by('-tally_updated')[:10]
+    elections = get_elections()
+    info = get_races_info(Race.objects.all())
+    return render_to_response('index.html', {'key_races': key, 'incoming_races': incoming, 'decided_races': info['decided_races'], 'green_races': info['green_races'], 'percent_green': info['percent_green'], 'remaining_races': info['remaining_races'], 'elections': elections,})
+
+def get_elections():
+    elections = []
+    for abbr, name in STATE_CHOICES:
+        info =get_races_info(Race.objects.filter(state=abbr))
+        elections.append((name, info['percent_green']))
+    return elections
+
+#TODO refactor meter so that it takes this dict??
+def get_races_info(races):
     total_races = 0
     decided_races = 0
     green_races = 0
-    for race in Race.objects.all():
+    for race in races:
         total_races += 1
         if race.winner:
             decided_races += 1
             if race.winner == race.greenest:
                 green_races += 1
-    percent_green = 100 * float(green_races) / float(decided_races)
+    if decided_races > 0:
+        percent_green = 100 * float(green_races) / float(decided_races)
+    else:
+        percent_green = 0
     remaining_races = total_races - decided_races
-    elections = get_elections()
-    return render_to_response('index.html', {'key_races': key, 'incoming_races': incoming, 'decided_races': decided_races, 'green_races': green_races, 'percent_green': percent_green, 'remaining_races': remaining_races, 'elections': elections,})
-
-def get_elections():
-    elections = []
-    for abbr, name in STATE_CHOICES:
-        total_races = 0
-        decided_races = 0
-        green_races = 0
-        races = Race.objects.filter(state=abbr)
-        for race in races:
-            total_races += 1
-            if race.winner:
-                decided_races += 1
-                if race.winner == race.greenest:
-                    green_races += 1
-        if decided_races > 0:
-            percent_green = 100 * float(green_races) / float(decided_races)
-        else:
-            percent_green = 0
-        remaining_races = total_races - decided_races
-        elections.append((name, percent_green,))
-    return elections
+    return {'decided_races': decided_races, 'green_races': green_races, 'percent_green': percent_green, 'remaining_races': remaining_races,}
+    
         
         
