4d59c1c3c4016c7c38c0896c211a3fbe5f27460c
benderbending <benderbending@aeaa4eaa-a835-11dd-a887-7ffe1a420f8d>
2008-11-16 01:33:30 +0000

    added last election info to race page, refactored race info into race_short templatetag, and made lots of little formatting tweaks.  also made a bunch of html valid.

--- trunk/envirovote/races/templatetags/race_extras.py
+++ trunk/envirovote/races/templatetags/race_extras.py
@@ -24,6 +24,10 @@ def show_medium_meter(meter_info):
 def show_state_race(race):
     return {'race': race}
 
+@register.inclusion_tag('race_short.html')
+def show_race_short(race):
+    return {'race': race}
+
 
 
 
